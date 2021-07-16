3a29be2bab06b54eecb7a96ac3e53739532fb515
arggh <>
2021-07-16 15:27:14 +0300

    Working setup with Svelte@3.8.1

---
+++ client/App.svelte
@@ -0,0 +1,11 @@
+<style lang="postcss">
+  .example {
+    padding: 20px;
+    background: env(--svelte-postcss-color);
+    color: gray(85);
+    font: 40px sans-serif;
+    backface-visibility: hidden;
+  }
+</style>
+
+<div class="example">Svelte & PostCSS</div>
